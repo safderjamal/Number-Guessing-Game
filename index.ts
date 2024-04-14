4fbc395d1310f32e9cb1f23a9d37feb7eeb74642
safderjamal <safderj2@gmail.com>
2024-04-14 05:15:22 +0500

    Created Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+console.log ("\n◄◄◄◄◄◄◄ NUMBER GUESSING GAME ►►►►►►►\n");
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt ([{
+    message: "Please Guess a Number between 1 - 6: ",
+    type: "number",
+    name: "userGuessedNumber",
+}])
+
+const randomNumber =  Math.floor( Math.random() * 6 +1);
+console.log("\nComputer Generated Number =", randomNumber);
+
+if (answer.userGuessedNumber === randomNumber)
+    {
+        console.log("\nCONGRATULATIONS !! You Guessed a Right Number");
+        console.log("YOU WON :)");
+    }
+
+    else 
+    {
+        console.log("\nOhh No !! You Guessed a Wrong Number");
+        console.log("TRY AGAIN..");
+    }
