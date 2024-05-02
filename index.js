ba47215416f7e30cb4deeeb9c930a06b1c77302c
safderjamal <safderj2@gmail.com>
2024-05-02 05:19:14 +0500

    Edit if else, put comments, correct spellings

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 #! /usr/bin/env node
+//Shebang
 console.log("\n◄◄◄◄◄◄◄ NUMBER GUESSING GAME ►►►►►►►\n");
+console.log("You have total 5 Attempts to guess the correct number");
 import inquirer from "inquirer";
 let attempt = 5;
+//Apply loop, user can try only 5 times
 while (attempt > 0) {
     const answer = await inquirer.prompt([{
-            message: `(Attemps Left ${attempt})\nPlease Guess a Number between 1 - 6 : `,
+            message: `(Attemps Left ${attempt})\nPlease guess a number between 1 - 6 : `,
             type: "number",
             name: "userGuessedNumber",
         }]);
+    //Formula to created the random number with round figure
     const randomNumber = Math.floor(Math.random() * 6 + 1);
     console.log("\nComputer Generated Number =", randomNumber);
     if (answer.userGuessedNumber === randomNumber) {
-        console.log("\nCONGRATULATIONS !! You Guessed a Right Number");
+        console.log("\nCONGRATULATIONS !! You Guessed The Correct Number");
         console.log("YOU WON :)");
         break;
     }
     else {
-        console.log("\nOhh No !! You Guessed a Wrong Number");
+        console.log("\nOhh No !! You Guessed The Wrong Number");
         if (attempt > 1) {
             console.log("TRY AGAIN..\n");
         }
+        else {
+            console.log("\nYOU LOST :(");
+            console.log("\nYOUR 5 ATTEMPTS COMPLETED!! TRY NEXT TIME");
+        }
     }
     attempt--;
-}
-if (attempt == 0) {
-    console.log("\nYOUR 5 ROUNDS COMPLETED!! TRY NEXT TIME");
 }
